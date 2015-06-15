fd7991084358cff9944bf1ebb29fdd193668dd87
varesh <g.varesh@gmail.com>
2015-06-15 23:32:55 +0530

    this is user scaffold commit to git master

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :Microposts
+  has_many :microposts
+  # validates :content, length: { maximum: 140 }, presence: true
+    validates :name,  length: {minimum: 8 , maximum: 15},presence: true
+
 end
