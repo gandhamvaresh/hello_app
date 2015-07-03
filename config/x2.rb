05d1bf402aabb392c6a22b6958126196ea179462
varesh <g.varesh@gmail.com>
2015-07-03 08:49:26 +0530

    code added.

--- config/x2.rb
+++ config/x2.rb
@@ -21,4 +21,37 @@ def paren(str)
 end
 p paren("{}{([)}")
 
+________________________________________________________________________________________________________
+
+def indices(x, y)
+  e =[]
+  x1 = x.split('')
+  y1 =y.split('')
+  f =0
+  if x.size>y.size
+    x1.each do |y2|
+      e << f if y1.include? y2
+      f = f+1
+
+    end
+  else # y.size > x.size
+    y1.each do |y2|
+      e << f if x1.include? y2
+      f = f+1
+    end
+  end
+  p e
+end
+
+
+
+indices('abc', 'axbydefrdec') #=> [0, 2, 10]
+indices('axbyc', 'abc') #=> [0, 2, 4]
+indices('ambnc', 'mn')# => [ 1, 3 ]
+
+
+_____________________________________________________________________________________________________
+
+
+
 
